cbe6225c79a37d3d252e6b3850e1ad9669515a7b
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-10-27 02:15:58 -0700

    Add files via upload

---
+++ lab manual 4 HOMEWORK by Muhammad Zaorez 455443.cpp
@@ -0,0 +1,46 @@
+/* 
+                            LAB MANUAL 04( HOME WORK)
+                             MUHAMMAD ZAOREZ
+                                 455443
+                                 SEC A 
+*/                                 
+#include <iostream>
+using namespace std;
+int main() 
+{
+//                                   HOME TASK 01
+/*for (int i = 1; i <= 150; i++) {
+    if (i % 10 == 0) {
+            continue;
+        }
+        cout << i << " ";
+    }
+
+    return 0;*/
+    
+//                              HOME TASK 02
+ /*int number, sum = 0;
+cout << "Enter a number: ";
+cin >> number;
+
+while (number != 0) {
+    sum += number % 10;
+    number /= 10;
+}
+
+cout << "The sum of the digits is: " << sum;
+
+return 0;*/
+
+//                               HOME TASK 03
+    
+    // unable to perform
+    
+    
+    
+    
+    
+    
+}
+
+
